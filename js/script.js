3aeaf38308b9631f000d8525c7a33b251650086e
Marta Zebrowska-Giers <zeberita@gmail.com>
2022-11-23 23:02:15 +0100

    changes in function

--- js/script.js
+++ js/script.js
@@ -39,14 +39,17 @@ const bindEvents = () => {
 
 const render = () => {
   let htmlString = "";
+
   for (const task of tasks) {
     htmlString += `
-    <li class="tasks__item ${task.done ? "tasks__content--done" : ""}"
+    <li class="tasks__item"
   >
   <button class="tasks__button tasks__button--done js-done"> ${
     task.done ? "✓" : ""
   }</button>
+  <span class="tasks__content${task.done ? "tasks__content--done" : ""}">
   ${task.content}
+  </span>
  
   <button class="tasks__button tasks__button--remove js-remove">🗑</button>
   </li>
